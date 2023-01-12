38be800449800bd00082866b6abd001639b1a073
ayu <chenurwahidaayu2000@gmail.com>
2023-01-13 00:12:51 +0800

    Add ADDNEWFRIEND Page

---
+++ app/src/main/java/com/example/youoweme/AddFriendActivity.java
@@ -0,0 +1,16 @@
+package com.example.youoweme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.youoweme.R;
+
+public class AddFriendActivity  extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+
+
+    }
+}
