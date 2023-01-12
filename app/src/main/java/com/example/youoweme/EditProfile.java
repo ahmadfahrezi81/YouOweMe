80bc7a58514319924472acf92bd984a9f57f100d
11arns <aidashakiman@gmail.com>
2023-01-13 04:00:48 +0800

    Button edit profile and log out

--- app/src/main/java/com/example/youoweme/EditProfile.java
+++ app/src/main/java/com/example/youoweme/EditProfile.java
@@ -2,14 +2,20 @@ package com.example.youoweme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditProfile extends AppCompatActivity {
+
+    Button saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-        getSupportActionBar().setTitle("Edit Profile");
+
+
     }
 }
