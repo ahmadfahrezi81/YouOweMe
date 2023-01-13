f6d2d453cbd05323821e3ab8ab6ba8eb81fc1578
11arns <aidashakiman@gmail.com>
2023-01-13 18:30:33 +0800

    add settle up

--- app/src/main/java/com/example/youoweme/AddFriendActivity.java
+++ app/src/main/java/com/example/youoweme/AddFriendActivity.java
@@ -1,7 +1,10 @@
 package com.example.youoweme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.youoweme.R;
 
@@ -10,7 +13,15 @@ public class AddFriendActivity  extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+        Button btnsettleup = findViewById(R.id.button2);
+        btnsettleup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+
+                startActivity(new Intent(AddFriendActivity.this, SettleUpFragment.class));
+            }
+        });
 
     }
 }
